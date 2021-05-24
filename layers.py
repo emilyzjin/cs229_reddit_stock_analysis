f17c94a7d8adfbe9c81482548b43b50a1e8d288b
Danny Tse <dannytse@stanford.edu>
2021-05-24 02:21:07 -0700

    Basic output layer (2 FC layers) complete.

--- layers.py
+++ layers.py
@@ -1,12 +1,43 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import pdb 
 
-class SequentialEncoder(nn.Module):
+class RNN_Encoder(nn.Module):
     """
-    Encode an input sequence, considering word order.
+    Encode an input sequence recurrently, considering word order.
     """
-    def __init__(self, num_layers, hidden_size):
-        super(SequentialEncoder, self).__init__()
-        
+    def __init__(self, num_layers, hidden_size, drop_prob):
+        super(RNN_Encoder, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
+
+
+class SentimentAnalysis(nn.Module):
+    """
+    Perform sentiment analysis on model. 
+    """
+    def __init__(self, num_layers, hidden_size, alpha, drop_prob):
+        super(SentimentAnalysis, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
+
+
+class OutputLayer(nn.Module):
+    """
+    Two-stack of fully-connected output layers with 7 neurons. 
+    To be used after sentiment analysis.
+    """
+    def __init__(self, input_size, hidden_size, alpha):
+        self.model = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ELU(alpha),
+            nn.Linear(hidden_size, 7),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        return self.model(x)
