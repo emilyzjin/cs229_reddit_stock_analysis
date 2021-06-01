7e067c8b409583fc61bfeb5d6273cdddc20f8a58
Tim <timwu0@stanford.edu>
2021-05-31 21:34:02 -0400

    get_available_devices

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import numpy as np
 import random
 import ujson as json
@@ -33,6 +34,22 @@ def get_words(tweet):
 
     return white_space_fix(remove_articles(lower(tweet))).split()
 
+def get_available_devices():
+    """Get IDs of all available GPUs.
+
+    Returns:
+        device (torch.device): Main device (GPU 0 or CPU).
+        gpu_ids (list): List of IDs of all GPUs that are available.
+    """
+    gpu_ids = []
+    if torch.cuda.is_available():
+        gpu_ids += [gpu_id for gpu_id in range(torch.cuda.device_count())]
+        device = torch.device(f'cuda:{gpu_ids[0]}')
+        torch.cuda.set_device(device)
+    else:
+        device = torch.device('cpu')
+
+    return device, gpu_ids
 
 def create_dict(tweets, num_occur):
     temp = {}
